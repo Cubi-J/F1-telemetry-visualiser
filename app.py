b30d24ff5e9cbe64c8135eaa24d73d1a6665ca3d
Ryan Jenkins <jenkinsr46@cardiff.ac.uk>
2026-03-21 23:09:53 +0000

    Improved Event / Driver Selection

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import dash
-from dash import dcc, html, Input, Output
+from dash import dcc, html, Input, Output, State
 import plotly.graph_objects as go
 import fastf1
 import polars as pl
 import os
+
+
+## HELPER METHODS
 
 ### Enable data chaching to reduce network load
 if not os.path.exists('cache'):
@@ -11,30 +14,8 @@ if not os.path.exists('cache'):
 
 fastf1.Cache.enable_cache('cache')
 
-session = fastf1.get_session(2025, 'Australia', 'R')
-session.load()
-
-# Fetch the driver's name, number, and abbreviation
-def get_driver_list():
-
-    driver_numbers = session.drivers
-    drivers = []
-    for num in driver_numbers:
-        driver = session.get_driver(num)
-        drivers.append({'name': driver['FullName'], 'number': 'num', 'value': driver['Abbreviation']})
-
-    return drivers
-
-# Extract the drivers name and abbreviation for dropdown
-def convert_drivers_to_dropdown(drivers):
-    options = []
-    for driver in drivers:
-        options.append({'label': driver['name'], 'value': driver['value']})
-
-    return options
-
 # Fetch the fastest lap
-def get_fastest_lap(driver_code):
+def get_fastest_lap(driver_code, session):
     laps = session.laps
     fastest_lap = laps.pick_drivers(driver_code).pick_fastest()
 
@@ -43,97 +24,232 @@ def get_fastest_lap(driver_code):
 # Fetch telemetry for a given lap
 def get_telemetry_for_lap(lap):
     telemetry = lap.get_telemetry()
-    df = pl.from_pandas(telemetry)
-
-    df = df.with_columns(
-        (pl.col("Time").dt.total_nanoseconds() / 1e9).alias("TimeSeconds")
-    )
-    return df
+    return telemetry
 
 
+## APPLICATION CODE
 app = dash.Dash(__name__)
 
-app.layout = html.Div(style={'backgroundColor': '#111111', 'color': '#00CED5', 'padding': '20px'},
+app.layout = html.Div(style={
+    'backgroundColor': '#111111', 'color': '#00CED5', 'padding': '20px',
+    },
     children=[
+
         html.H1("F1 Telemetry", style={'textAlign': 'center', 'color': "#00CED5"}),
 
-        html.Div(style={'display': 'flex', 'gap': '20px', 'marginBottom': '20px'}, children=[
+        dcc.Store(id='session-store'),
+
+        # Header
+        html.Div(style={
+            'display': 'flex', 
+            'flexWrap': 'wrap', 
+            'gap': '20px', 
+            'padding': '15px', 
+            'backgroundColor': '#1a1a1a', 
+            'borderRadius': '10px',
+            'marginBottom': '20px',
+            'alignItems': 'end'
+            }, children=[
+
+            # Year selection
+            html.Div([
+                html.Label("YEAR", style={'fontSize': '10px', 'display': 'block'}),
+                dcc.Dropdown(
+                    id='year-dropdown',
+                    options=[{'label': str(yr), 'value': yr} for yr in range (2018, 2027)],
+                    value=2025,
+                    clearable=False,
+                    style={'width': '200px', 'color': 'black'}
+                ),
+            ]),
+
+            # Event Selection
+            html.Div([
+                html.Label("GRAND PRIX", style={'fontSize': '10px', 'display': 'block'}),
+                dcc.Dropdown(id='event-dropdown', placeholder="Select Event", options=[], clearable=False, style={'width': '200px', 'color': 'black'})
+            ]),
+
+            # Session Selection
+            html.Div([
+                html.Label("SESSION", style={'fontSize': '10px', 'display': 'block'}),
+                dcc.Dropdown(
+                    id='session-dropdown', 
+                    options=[
+                        {'label': 'Practice 1', 'value': 'FP1'},
+                        {'label': 'Practice 2', 'value': 'FP2'},
+                        {'label': 'Practice 3', 'value': 'FP3'},
+                        {'label': 'Qualifying', 'value': 'Q'},
+                        {'label': 'Race', 'value': 'R'}
+                    ], 
+                    placeholder="Select Session", 
+                    clearable=False, 
+                    style={'width': '120px', 'color': 'black'}
+                )
+            ]),
+
+            # Driver Selection
             html.Div([
                 html.Label("SELECT DRIVER", style={'color': '#00CED5', 'fontSize': '12px'}),
                 dcc.Dropdown(
                     id='driver-dropdown',
-                    options=convert_drivers_to_dropdown(get_driver_list()),
-                    value='VER',
+                    options=[],
+                    placeholder="Select Driver",
+                    clearable=False,
                     style={'width': '200px', 'color': 'black'}
                 ),
             ]),
+
+            # Apply Selections
+            html.Button('APPLY CHANGES', id='apply-btn', n_clicks=0, style={
+                'backgroundColor': '#00CED5', 
+                'color': '#111', 
+                'border': 'none', 
+                'padding': '10px 20px', 
+                'fontWeight': 'bold',
+                'cursor': 'pointer',
+                'borderRadius': '5px'
+            })
+
         ]),
-      
-        dcc.Graph(
-            id='live-telemetry-graph',
-            responsive=True,
-            figure={
-                'data': [],
-                'layout': {
-                    'title': 'Select a driver to view telemetry',
-                    'plot_bgcolor': '#111111',
-                    'paper_bgcolor': '#111111',
-                    'font': {'color': '#00CED5'},
-                    'xaxis': {
-                        'gridcolor': '#333', 
-                        'title': {'text': 'Distance (m)', 'font': {'color': '#00CED5'}},
-                        },
-                    'yaxis': {
-                        'gridcolor': '#333', 
-                        'title': {'text': 'Speed (km/h)', 'font': {'color': '#00CED5'}}
-                        }
-                }   
-            }
-        )
+        
+        dcc.Loading(
+            id="loading-telemetry",
+            type="graph",
+            children=
+            dcc.Graph(
+                id='live-telemetry-graph',
+                responsive=True,
+                figure={
+                    'data': [],
+                    'layout': {
+                        'title': 'Select a driver to view telemetry',
+                        'plot_bgcolor': '#111111',
+                        'paper_bgcolor': '#111111',
+                        'font': {'color': '#00CED5'},
+                        'xaxis': {
+                            'gridcolor': '#333', 
+                            'title': {'text': 'Distance (m)', 'font': {'color': '#00CED5'}},
+                            },
+                        'yaxis': {
+                            'gridcolor': '#333', 
+                            'title': {'text': 'Speed (km/h)', 'font': {'color': '#00CED5'}}
+                            }
+                    }   
+                }
+            ),
+        ),
     ])
 
+# Callbacks
+# Update events based on chosen year
+@app.callback(
+        Output('event-dropdown', 'options'),
+        Input('year-dropdown', 'value')
+)
+def update_events_dropdown(year):
+    print(f"Updating Events Dropdown...")
+    
+    # Get Events for given year
+    events = fastf1.get_event_schedule(year)
+    options = []
+    for index, row in events.iterrows():
+        options.append(row.EventName)
+
+    return options
+
+# Update drivers based stored session
+@app.callback(
+        Output('driver-dropdown', 'options'),
+        [Input('year-dropdown', 'value'),
+        Input('event-dropdown', 'value'),
+        Input('session-dropdown', 'value')]
+)
+def update_driver_dropdown(year, event, session_type):
+    if not year or not event or not session_type:
+        return []
+    
+    print(f"Pre-loading drivers for {event} {year}...")
+
+    try:
+        session = fastf1.get_session(year, event, session_type)
+        session.load(laps=True, telemetry=False, weather=False, messages=False)
+
+        driver_options = []
+        for drv in session.drivers:
+            info = session.get_driver(drv)
+            driver_options.append({
+                'label': f"{info['FullName']} ({info['Abbreviation']})", 
+                'value': info['Abbreviation']
+            })
+        
+        return driver_options
+
+    except Exception as e:
+        print(f"Error loading drivers: {e}")
+        return [], None
+
+# Sync all dropdown choices to store
+@app.callback(
+    Output('session-store', 'data'),
+    Input('apply-btn', 'n_clicks'),
+    State('year-dropdown', 'value'),
+    State('event-dropdown', 'value'),
+    State('session-dropdown', 'value'),
+    State('driver-dropdown', 'value'),
+    prevent_initial_call=True
+)
+def sync_to_store(n_clicks, year, event, session, driver):
+    if not all([year, event, session, driver]):
+        return dash.no_update
+    
+    return {
+        'year': year, 
+        'event': event, 
+        'session': session, 
+        'driver': driver
+    }
 
 
 # Update the graph
 @app.callback(
     Output('live-telemetry-graph', 'figure'),
-    Input('driver-dropdown', 'value')
+    Input('session-store', 'data'),
 )
-def update_graph(selected_driver):
-    print(f"Loading telemetry for: {selected_driver}")
+def update_graph(session_info):
+    
+    if not session_info:
+        return dash.no_update
+    
+    # Reload session
+    session = fastf1.get_session(session_info['year'], session_info['event'], session_info['session'])
+    session.load(telemetry=True, laps=True, weather=False)
+    
+    driver = session_info['driver']
 
     # Get telemetry
-    telemetry = get_telemetry_for_lap(get_fastest_lap(selected_driver))
+    telemetry = get_telemetry_for_lap(get_fastest_lap(driver, session))
     if telemetry is None:
         return dash.no_update
     
-    trace = go.Scatter(
-        x=telemetry['TimeSeconds'],
+    fig = go.Figure()
+    # Draw graph    
+    fig.add_trace(go.Scatter(
+        x=telemetry['Distance'],
         y=telemetry['Speed'],
-        mode='lines',
-        name=f'Speed over Distance - {selected_driver}',
+        name=f'Speed over Distance - {driver}',
         line={'color': '#00CED5'}
+    ))
+
+    fig.update_layout(
+        title=f"Fastest Lap Telemetry: {driver}",
+        template="plotly_dark",
+        plot_bgcolor='#111111',
+        paper_bgcolor='#111111',
+        xaxis_title="Distance (m)",
+        yaxis_title="Speed (kn/h)"
     )
 
-    return {
-        'data': [trace],
-        'layout': {
-            'title': {'text': f'{selected_driver}',
-                      'font': {'color': '#00CED5', 'size': 20}
-                      },
-            'plot_bgcolor': '#111111',
-            'paper_bgcolor': '#111111',
-            'font': {'color': '#00CED5'},
-            'xaxis': {
-                'gridcolor': '#333', 
-                'title': {'text': 'Distance (m)', 'font': {'color': '#00CED5'}},
-                },
-            'yaxis': {
-                'gridcolor': '#333', 
-                'title': {'text': 'Speed (km/h)', 'font': {'color': '#00CED5'}}
-                }
-        }   
-    }
+    return fig
 
 if __name__ == '__main__':
     app.run(debug=True)
